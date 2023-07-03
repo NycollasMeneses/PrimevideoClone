b9c46a03e8da3a2ff0a42753437ea48575582769
NycollasMeneses <117913596+NycollasMeneses@users.noreply.github.com>
2023-07-03 18:50:53 -0300

    movie.js

--- movie.js
+++ movie.js
@@ -13,16 +13,49 @@ const fetchData = (url, limit, category) => {
           if (index === 1) {
             main.innerHTML += `
               <h2><span style="color: blue;">${category}</span> Amazon Originais e exclusivos</h2>
-              <div class="card movie"><img src="https://image.tmdb.org/t/p/w500${item.poster_path}"/></div>
+              <div class="card movie" data-title="${item.title}" data-description="${item.overview}">
+                <img src="https://image.tmdb.org/t/p/w500${item.poster_path}"/>
+              </div>
             `;
           } else {
             main.innerHTML += `
-              <div class="card movie"><img src="https://image.tmdb.org/t/p/w500${item.poster_path}"/></div>
+              <div class="card movie" data-title="${item.title}" data-description="${item.overview}">
+                <img src="https://image.tmdb.org/t/p/w500${item.poster_path}"/>
+              </div>
             `;
           }
         }
       });
+
+      const movieCards = document.querySelectorAll('.card.movie');
+      movieCards.forEach(card => {
+        card.addEventListener('mouseenter', showMovieDescription);
+        card.addEventListener('mouseleave', hideMovieDescription);
+      });
     });
+};
+
+const showMovieDescription = (event) => {
+  const card = event.target;
+  const title = card.getAttribute('data-title');
+  const description = card.getAttribute('data-description');
+  
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+  popup.innerHTML = `
+    <h3>${title}</h3>
+    <p style="color: white;">${description}</p>
+  `;
+  
+  card.appendChild(popup);
+};
+
+const hideMovieDescription = (event) => {
+  const card = event.target;
+  const popup = card.querySelector('.popup');
+  if (popup) {
+    card.removeChild(popup);
+  }
 };
 
 fetchData('https://api.themoviedb.org/3/movie/popular?api_key=95bf46ca4de17d681a30e5c223fef7f4&language=en-US&page=1', 16, 'Prime');
